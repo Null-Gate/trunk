4e176e425516e059e6ef6d6ff2699417ce8ef539
Walker-00 <w000alker@gmail.com>
2024-01-22 14:45:27 +0630

    Http Server inited

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use actix_web::{HttpServer, App};
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| App::new()).bind(("127.0.0.1", 8090))?.run().await
 }

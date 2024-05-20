61e1aca30453c4bb385f39793b3edfeb7fa3a565
Linus Walker <rissk_it@proton.me>
2024-05-20 20:48:07 +0629

    fix: fixed some issues

--- backend/src/fetch/nf.rs
+++ backend/src/fetch/nf.rs
@@ -1,7 +1,7 @@
 use tokio_tungstenite::tungstenite::Result;
 
 use crate::{
-    extra::wserror, structures::{DbCarPost,  NewFeed, DB}
+    extra::wserror, structures::{DbCarPost, NewFeed, Post, DB}
 };
 
 pub async fn fetch_newfeed() -> Result<NewFeed, tokio_tungstenite::tungstenite::Error> {
@@ -14,11 +14,11 @@ pub async fn fetch_newfeed() -> Result<NewFeed, tokio_tungstenite::tungstenite::
     let packagesql = "SELECT * FROM package ORDER BY RAND() LIMIT 50;";
 
     let mut ret = db.query(car_postsql).query(packagesql).await.unwrap();
-    let db_car_posts = ret.take::<Vec<DbCarPost>>(0).unwrap();
+    let db_car_posts = ret.take::<Vec<Post<DbCarPost>>>(0).unwrap();
     let mut car_posts = vec![];
 
     for x in db_car_posts {
-        car_posts.push(x.to_resp().await.unwrap());
+        car_posts.push(x.to_resp().await);
     }
 
     Ok(NewFeed {
--- backend/src/services/voting.rs
+++ backend/src/services/voting.rs
@@ -1,7 +1,8 @@
 use actix_web::{post, web::Path, HttpResponse};
+use serde_json::Value;
 use surrealdb::sql::{Id, Thing};
 
-use crate::{extra::{check_user, decode_token, internal_error, verify_password}, structures::{VRelate, Vote, DB}};
+use crate::{extra::{check_user, decode_token, internal_error, verify_password}, structures::{Post, VRelate, Vote, DB}};
 
 #[allow(clippy::future_not_send)]
 #[post("/up/{p_id}/{token}")]
@@ -11,22 +12,45 @@ pub async fn up_vote(paths: Path<(String, String)>) -> HttpResponse {
         return internal_error(e);
     }
 
-    match decode_token(&paths.0) {
+    match decode_token(&paths.1) {
         Ok(user_info) => {
             match check_user(user_info.username.clone(), db).await {
                 Ok(user) => {
                     match verify_password(&user_info.password, &user.password) {
                         Ok(()) => {
                             let cuery = "SELECT * FROM vote WHERE (in = type::thing($uthing) AND out = type::thing($pthing));";
-                            let mut idk = db.query(cuery).bind(("uthing", Thing{
+                            let iuery = "SELECT * FROM type::thing($pthing);";
+                            let mut idk = db.query(cuery).query(iuery).bind(("uthing", Thing{
                                 tb: "user".into(),
                                 id: Id::from(user_info.username.to_string())
+                            })).bind(("pthing", Thing {
+                                tb: "user".into(),
+                                id: Id::from(&paths.0)
                             })).await.unwrap();
                             
                             let sel: Option<VRelate> = idk.take(0).unwrap();
+                            let iid: Option<Post<Value>> = idk.take(1).unwrap();
 
-                            if let Some(voted) = sel {
-                                if voted.vote == Vote::Up {}
+                            if let (Some(voted), Some(mut post)) = (sel, iid) {
+                                if voted.vote == Vote::Up {
+                                    post.votes -= 1;
+                                    let duery = "DELETE type::thing($uthing)->vote WHERE out = type::thing($pthing);";
+                                    db.query(duery).bind(("uthing", Thing{
+                                        tb: "user".into(),
+                                        id: Id::from(user_info.username.to_string())
+                                    })).bind(("pthing", Thing {
+                                        tb: "user".into(),
+                                        id: Id::from(&paths.0)
+                                    })).await.unwrap();
+
+                                     match db.update::<Option<Value>>(("post", Id::from(&paths.0))).await {
+                                         Ok(Some(_)) => todo!(),
+                                         Ok(None) => internal_error("None Update Vote Post Error!"),
+                                         Err(e) => internal_error(e)
+                                     };
+                                     todo!()
+                                }
+                                todo!()
                             };
 
                             //let _query = "RELATE type::thing($uthing)->vote->type::thing($pthing) SET vote = 'up';";

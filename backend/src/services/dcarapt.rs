720b7fc1264c60879208b981966adc2874b8d106
Linus Walker <rissk_it@proton.me>
2024-07-06 15:38:49 +0629

    feat: done dcarapt

--- backend/src/services/dcarapt.rs
+++ backend/src/services/dcarapt.rs
@@ -1,5 +1,5 @@
 use actix_web::{put, web::Path, HttpResponse};
-use surrealdb::sql::Id;
+use surrealdb::{opt::PatchOp, sql::Id};
 
 use crate::{
     extra::functions::{ct_user, internal_error},
@@ -25,7 +25,7 @@ pub async fn driver_acpt_car(pdata: Path<(String, String)>) -> HttpResponse {
     match ct_user(token).await {
         Ok((_, duser)) => {
             match db
-                .delete::<Option<Noti<CargoD>>>((&duser.username, Id::String(id.clone())))
+                .select::<Option<Noti<CargoD>>>((&duser.username, Id::String(id.clone())))
                 .await
                 .unwrap()
             {
@@ -41,10 +41,12 @@ pub async fn driver_acpt_car(pdata: Path<(String, String)>) -> HttpResponse {
                         .unwrap()
                         .unwrap();
                     db.create::<Option<PostD<DbCarInfo>>>(("post", Id::String(id.clone())))
-                        .content(nt.data.pdata)
+                        .content(&nt.data.pdata)
                         .await
                         .unwrap()
                         .unwrap();
+                    db.update::<Option<DbCarInfo>>(("car", id.clone())).patch(PatchOp::replace("is_available", true)).await.unwrap().unwrap();
+                    db.create::<Option<CargoD>>(("dond", Id::String(duser.username))).content(nt).await.unwrap().unwrap();
                     HttpResponse::Ok().await.unwrap()
                 }
                 None => HttpResponse::NoContent().await.unwrap(),
